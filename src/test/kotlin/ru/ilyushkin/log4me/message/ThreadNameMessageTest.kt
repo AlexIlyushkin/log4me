7039cb1cb97c605ea69c12f5a27fe78626b11235
AlexIlyushkin <alexilyushkin2000@gmail.com>
2022-01-08 13:31:24 +0400

    Added tests for ThreadNameMessage.equals(Any)

--- src/test/kotlin/ru/ilyushkin/log4me/message/ThreadNameMessageTest.kt
+++ src/test/kotlin/ru/ilyushkin/log4me/message/ThreadNameMessageTest.kt
@@ -1,10 +1,14 @@
 package ru.ilyushkin.log4me.message
 
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 /**
  * @author Alex Ilyushkin
@@ -13,8 +17,9 @@ import kotlin.test.assertEquals
 class ThreadNameMessageTest {
 
     @Test
-    @DisplayName("asString(): concatenates current thread name " +
-            "and result of origin message `asString()` method and returns concatenated string"
+    @DisplayName(
+        """asString(): concatenates current thread name 
+           and result of origin message `asString()` method and returns concatenated string"""
     )
     fun `asString() test`() {
         val messageAsString = "It's message `asString()` method result"
@@ -32,5 +37,66 @@ class ThreadNameMessageTest {
             expected = expectedResultAsString,
             actual = resultAsString
         )
+    }
+
+    @Test
+    @DisplayName("equals(Any): returns true on itself comparing")
+    fun `equals(Any) test`() {
+        val message = mockk<Message>()
+
+        val threadNameMessage = ThreadNameMessage(
+            origin = message
+        )
+
+        assertTrue { threadNameMessage.equals(threadNameMessage) }
+        confirmVerified(message)
+    }
+
+    @Test
+    @DisplayName("equals(Any): returns false if an object of another type provided as parameter")
+    fun `equals(Any) for object of another type`() {
+        val message = mockk<Message>()
+
+        val threadNameMessage = ThreadNameMessage(
+            origin = message
+        )
+
+        assertTrue { threadNameMessage != Any() }
+        confirmVerified(message)
+    }
+
+    @Test
+    @DisplayName("equals(Any): returns true if compared ThreadNameMessage have the same underlying messages")
+    fun `equals(Any) for the same underlying messages`() {
+        val message = mockk<Message>()
+
+        val threadNameMessage = ThreadNameMessage(
+            origin = message
+        )
+        val threadNameMessage1 = ThreadNameMessage(
+            origin = message
+        )
+
+        assertTrue { threadNameMessage == threadNameMessage1 }
+        verify(exactly = 1) { message == message }
+        confirmVerified(message)
+    }
+
+    @Test
+    @DisplayName("equals(Any): returns false if compared ThreadNameMessages have different underlying messages")
+    fun `equals(Any) for different underlying messages`() {
+        val message = mockk<Message>()
+        val message1 = mockk<Message>()
+
+        val threadNameMessage = ThreadNameMessage(
+            origin = message
+        )
+        val threadNameMessage1 = ThreadNameMessage(
+            origin = message1
+        )
+
+        assertFalse { threadNameMessage == threadNameMessage1 }
+        verify(exactly = 1) { message != message1 }
+        confirmVerified(message, message1)
     }
 }
